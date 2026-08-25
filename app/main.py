7c9f8c1f7ac74df9ee8d82aa28b635b952592b81
Oleg <olefinibrabus@gmail.com>
2026-08-25 11:31:39 +0300

    added functions copy_file with validations

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    s_command = command.split()
+    if not command or s_command[0] != "cp":
+        return
+
+    try:
+        f_file, s_file = s_command[1], s_command[2]
+    except IndexError:
+        return
+
+    if f_file == s_file:
+        return
+    try:
+        with open(f_file, "r") as f_in, open(s_file, "w") as f_out:
+            for line in f_in:
+                f_out.write(line)
+    except FileNotFoundError:
+        return
